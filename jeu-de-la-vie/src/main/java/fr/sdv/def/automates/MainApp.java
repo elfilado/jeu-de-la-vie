287abb90f7ee6324fdda839e1a465ee5bfe834d3
Dorian EL FILALI <94066103+elfilado@users.noreply.github.com>
2022-11-18 23:55:38 +0100

    Update MainApp.java

--- jeu-de-la-vie/src/main/java/fr/sdv/def/automates/MainApp.java
+++ jeu-de-la-vie/src/main/java/fr/sdv/def/automates/MainApp.java
@@ -3,18 +3,17 @@ package fr.sdv.def.automates;
 public class MainApp {
     public static void main( String[] args ) throws InterruptedException {
         System.out.println("Starting the game...\n");
-        System.out.println("Tour 1\n");
 
         Grille grille = new Grille(5,10);
         grille.init();
         JeuDeLaVie jeu = new JeuDeLaVie(grille);
 
         for (int i = 0; i < 5; i++) {
+            System.out.println("Tour" + (i+1) + "\n");
             jeu.afficherGrille();
             jeu.passerUnTour();
-            System.out.println("Tour" + i + "\n");
-            System.out.print("");
             Thread.sleep(2000);
+            System.out.print("");
         }
 
         System.out.println( "Ending the game...\n");
